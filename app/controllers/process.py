385c76d3c13c40c246435d933584612a4b77e2ab
mastortosa <modestomastortosa@gmail.com>
2014-04-08 21:14:55 +0100

    change twitter processing followers

--- app/controllers/process.py
+++ app/controllers/process.py
@@ -1,8 +1,9 @@
 import json
 from celery import Celery, chain
 from tornado.web import RequestHandler
+from neo4jrestclient import client
 from app.settings import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
-from app.utils import filter_dict
+from app.utils import filter_dict, dict_to_neo4j_str
 
 # debug__
 from time import time
@@ -16,17 +17,21 @@ TWITTER_LABEL = 'TwitterUser'
 
 def get_twitter_user_node(user_id, db):
     """ Returns a neo4j node from a node_id. """
-    return db.labels[TWITTER_LABEL].get(id=user_id)
+    try:
+        return db.labels[TWITTER_LABEL].get(id=user_id)[0]
+    except:
+        return None
 
 
-def get_or_create_twitter_user_node(user_dict, db):
-    """ Returns a neo4j node from a user dict from twitter api response. """
-    user_dict = filter_dict(user_dict, TWITTER_FIELDS)
-    user_node = db.labels[TWITTER_LABEL].get(id=user_dict['id'])
-    if not user_node:
-        user_node = db.node(**user_dict)
-        user_node.labels.add(TWITTER_LABEL)
-    return user_node
+# def get_or_create_twitter_user_node(user_dict, db):
+#     """ Returns a neo4j node from a user dict from twitter api response. """
+#     user_dict = filter_dict(user_dict, TWITTER_FIELDS)
+#     user_node = get_twitter_user_node(user_dict['id'], db)
+#     if not user_node:
+#         q = 'create (n:{label} {properties}) return n'.format(
+#             label=TWITTER_LABEL, properties=dict_to_neo4j_str(user_dict))
+#         user_node = db.query(q, returns=(client.Node,))[0][0]
+#     return user_node
 
 
 @celery_app.task
@@ -41,20 +46,48 @@ def process_facebook_likes(user_id, like_list, db):
 
 @celery_app.task
 def process_twitter_followers(user_id, follower_list, db):
+    print 'processing twitter followers'
     user_node = get_twitter_user_node(user_id, db)
-    with db.transaction() as _:
+    with db.transaction(for_query=True) as _:
         for i in follower_list:
-            i_node = get_or_create_twitter_user_node(i, db)
-            i_node.follows(user_node)
+            i = filter_dict(i, TWITTER_FIELDS)
+            i_node = get_twitter_user_node(i['id'], db)
+            if i_node:
+                q = 'start x=node(%s), y=node(%s) create (x)<-[:%s]-(y)' % (
+                    user_node.id,
+                    i_node.id,
+                    'follows')
+            else:
+                q = 'start x=node(%s) create (x)<-[:%s]-(y:%s %s)' % (
+                    user_node.id,
+                    'follows',
+                    TWITTER_LABEL,
+                    dict_to_neo4j_str(i))
+            db.query(q)
+            print 'TwitterUser node created'
 
 
 @celery_app.task
 def process_twitter_following(user_id, following_list, db):
+    print 'processing twitter followings'
     user_node = get_twitter_user_node(user_id, db)
-    with db.transaction() as _:
+    with db.transaction(for_query=True) as _:
         for i in following_list:
-            i_node = get_or_create_twitter_user_node(i, db)
-            user_node.follows(i_node)
+            i = filter_dict(i, TWITTER_FIELDS)
+            i_node = get_twitter_user_node(i['id'], db)
+            if i_node:
+                q = 'start x=node(%s), y=node(%s) create (x)-[:%s]->(y)' % (
+                    user_node.id,
+                    i_node.id,
+                    'follows')
+            else:
+                q = 'start x=node(%s) create (x)-[:%s]->(y:%s %s)' % (
+                    user_node.id,
+                    'follows',
+                    TWITTER_LABEL,
+                    dict_to_neo4j_str(i))
+            db.query(q)
+            print 'TwitterUser node created'
 
 
 class FacebookProcess(RequestHandler):
@@ -91,7 +124,7 @@ class TwitterProcess(RequestHandler):
     def post(self):
         # debug__
         print 'POST'
-        print 'processing facebook...'
+        print 'processing twitter...'
         _time = time()
         print '  t = 0'
         # __debug
--- app/utils.py
+++ app/utils.py
@@ -1,2 +1,10 @@
+import json
+import re
+
+
 def filter_dict(d, fields):
     return dict((k, v) for k, v in d.items() if k in fields)
+
+
+def dict_to_neo4j_str(d):
+    return re.sub(r'\"(.*?)\":', r'\1:', json.dumps(d))
